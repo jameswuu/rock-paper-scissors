24a8b6c930ba078873b9febc97447206c4317526
James Wu <jameswlb01@gmail.com>
2024-11-12 18:24:44 +0800

    Add function to play one round of rock-paper-scissor

--- myscript.js
+++ myscript.js
@@ -3,19 +3,18 @@ function getComputerChoice() {
     let num = Math.random();
     let choice = null;
     if (num >= 0 && num <=0.33) {
-        choice = "Rock";
+        choice = "rock";
     } else if (num > 0.33 && num<=0.66) {
-        choice = "Paper";
+        choice = "paper";
     } else {
-        choice = "Scissor"
+        choice = "scissor"
     }
-    console.log(num);
-    console.log(`The computer has chose: ${choice}`);
+    return choice;
 }
 
 
 // Prompt user for input
-function getHumamChoice() {
+function getHumanChoice() {
     let answer = prompt("What's your choice: Rock, Paper, Scissor?").toLowerCase();
     let selection = ["rock", "paper", "scissor"];
     
@@ -29,6 +28,37 @@ function getHumamChoice() {
     } 
     // Print to console to comfirm the user's answer
     else {
-        return console.log(`The user has chose: ${answer}`);
+        return answer;
     }
 }
+
+
+// Create a function to play one round
+function playRound(human, computer) {
+    // Dubugging line
+    console.log(`The human has answer ${human} and computer is ${computer}` );
+
+    // if the human and computer has the same choice
+    if (human === computer) {
+        console.log(`Draws!`)
+    } 
+    // User wins
+    else if ( (human === "rock" && computer === "scissor" ) || (human === "paper" && computer === "rock") || 
+              (human === "scissor" && computer === "paper") ) {
+        console.log(`You win! ${human} beats ${computer}`);
+    } else {
+        console.log(`You lose! ${computer} beats ${human}`);
+    }
+}
+
+
+// Initalize the score for player and computer
+let humanScore = 0;
+let computerScore = 0;
+
+
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+console.log(`${humanSelection} and ${computerSelection}`)
+
+playRound(humanSelection, computerSelection);
