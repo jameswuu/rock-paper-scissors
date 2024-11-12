6659a35e0f7068b6a9ab1c914b850d8628e9edda
James Wu <jameswlb01@gmail.com>
2024-11-12 15:07:23 +0800

    Add Human choice functino to JS

--- myscript.js
+++ myscript.js
@@ -10,5 +10,25 @@ function getComputerChoice() {
         choice = "Scissor"
     }
     console.log(num);
-    console.log(choice);
+    console.log(`The computer has chose: ${choice}`);
+}
+
+
+// Prompt user for input
+function getHumamChoice() {
+    let answer = prompt("What's your choice: Rock, Paper, Scissor?").toLowerCase();
+    let selection = ["rock", "paper", "scissor"];
+    
+    // To ensure the input is not empty 
+    if (!answer){
+        return console.log(`Input cannot be empty`);
+    } 
+    // To valid the answer with the selection list
+    else if (!selection.includes(answer)) {
+        return console.log(`Invalid Input: ${answer}`);
+    } 
+    // Print to console to comfirm the user's answer
+    else {
+        return console.log(`The user has chose: ${answer}`);
+    }
 }
